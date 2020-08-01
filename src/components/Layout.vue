0c1244e39af29ff88db9e22b03ac30337c47c650
Adrijan <adrijan9@gmail.com>
2020-08-02 00:50:10 +0200

    added layout and sidebar

---
+++ src/components/Layout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="layout">
+    <div class="layout-sidebar">
+        sidebar
+    </div>
+    <div class="layout-content">
+        <div class="layout-header">
+            header`
+        </div>
+        <div class="layout-body">
+            body
+        </div>
+        <div v-if="$slots.footer"
+             class="layout-footer"
+        >
+            footer
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import "../scss/index.scss";
+  export default {
+    props: {
+    },
+
+    methods: {
+    }
+  };
+</script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      asdasd
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+  },
+
+  methods: {
+  }
+};
+</script>
+
+<style>
+
+</style>
