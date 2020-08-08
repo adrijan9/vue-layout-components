be02c89688e88a7c2f9203d64b1f943bd5bff074
Adrijan <adrijan9@gmail.com>
2020-08-08 23:18:40 +0200

    added layout header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <Layout>
-
-  </Layout>
+	<layout title="Dashboard"
+	>
+		<template #sidebar>
+			<sidebar-menu-item title="Item 1"
+			                   icon="someIcon"
+			                   submenu-size="full-screen"
+			>
+				<h2>123</h2>
+			</sidebar-menu-item>
+			<sidebar-menu-item title="Item 2"
+			                   icon="someIcon"
+			                   submenu-size="medium"
+			>
+				<h2>456</h2>
+			</sidebar-menu-item>
+		</template>
+	</layout>
 </template>
 
 <script>
-    import Layout from "./components/Layout";
-    export default {
-        name: "app",
-        components: {
-            Layout
-        }
-    }
+	import Layout from "./components/Layout";
+	import SidebarMenuItem from "./components/Menu/SidebarMenuItem";
+
+	export default {
+		name: "app",
+		components: {
+			Layout,
+			SidebarMenuItem
+		}
+	}
 </script>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,31 +1,65 @@
 <template>
-  <div class="layout">
-    <div class="layout-sidebar">
-        sidebar
-    </div>
-    <div class="layout-content">
-        <div class="layout-header">
-            header`
-        </div>
-        <div class="layout-body">
-            body
-        </div>
-        <div v-if="$slots.footer"
-             class="layout-footer"
-        >
-            footer
-        </div>
-    </div>
-  </div>
+	<div class="layout">
+		<sidebar-menu :show="isSidebarOpen">
+			<slot name="sidebar"></slot>
+		</sidebar-menu>
+		<div class="layout-content">
+			<div class="layout-header">
+				<button class="layout-header-toggle"
+				        @click="isSidebarOpen = !isSidebarOpen"
+				>
+					<span></span>
+					<span></span>
+					<span></span>
+				</button>
+				<div class="layout-header-holder">
+					<div class="layout-header-left">
+						<slot name="headerLeft" v-if="$slots.headerLeft"></slot>
+						<h3 v-if="title">
+							<i v-if="icon" :class="icon"></i>
+							{{ title }}
+						</h3>
+					</div>
+					<div class="layout-header-right">
+						<slot name="headerRight" v-if="$slots.headerRight"></slot>
+					</div>
+				</div>
+			</div>
+			<div class="layout-body">
+				body
+			</div>
+			<div v-if="$slots.footer"
+			     class="layout-footer"
+			>
+				footer
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-  import "../scss/index.scss";
-  export default {
-    props: {
-    },
+	import "../scss/index.scss";
+	import SidebarMenu from "./Menu/SidebarMenu";
 
-    methods: {
-    }
-  };
+	export default {
+		components: {
+			SidebarMenu
+		},
+		props: {
+			title: {
+				type: String,
+				default: null
+			},
+			icon: {
+				type: String,
+				default: null
+			}
+		},
+		data: function(){
+			return {
+				isSidebarOpen: false
+			};
+		},
+		methods: {}
+	};
 </script>
