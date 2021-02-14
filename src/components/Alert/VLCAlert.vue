48a5173b78f1dafd93b4d72c4acc95a0a955b182
Adrijan <adrijan9@gmail.com>
2021-02-14 14:00:52 +0100

    alert component

---
+++ src/components/Alert/VLCAlert.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-if="show" :class="['alert', `${type ? type : ''}`]">
+    <div class="alert-message">
+      <slot></slot>
+    </div>
+    <div v-if="dismissible"
+         class="alert-operations"
+    >
+      <button class="close"
+              @click="dismiss"
+      >
+        <i class="icon-close"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "VLCAlert",
+    props: {
+      show: {
+        type: Boolean,
+        default: true
+      },
+      type: {
+        type: String,
+        default: null
+      },
+      dismissible: {
+        type: Boolean,
+        default: false
+      }
+    },
+    emits: [
+      "dismiss"
+    ],
+    methods: {
+      dismiss () {
+        this.$emit("dismiss");
+      }
+    }
+  }
+</script>
